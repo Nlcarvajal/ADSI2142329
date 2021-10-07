889086ea90c8e441d925185863ab4b5b7a80b9c4
Nlcarvajal <nlcarvajal0@misena.edu.co>
2021-10-07 02:42:36 -0500

    portafolio v3

---
+++ portafolio/vista/inicio.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" href="../imagenes/icono.png" type="image/png"/>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>portafolio</title>
+  <link rel="stylesheet" href="../estilo/inicio.css">
+</head>
+<body background="../imagenes/fondo1.jpg" width="400%">
+<div class="wrap">
+<span class="decor"></span>
+<nav>
+  <ul class="primary">
+    <li>
+      <a href="">Inicio</a>
+      <ul class="sub">
+      </ul>
+    </li>
+    <li>
+      <a href="">Proyectos</a>
+      <ul class="sub">
+      </ul>  
+    </li>
+    <li>
+      <a href="">lenguajes</a>
+      <ul class="sub">
+        <li><a href="">HTml</a></li>
+        <li><a href="">CSS3</a></li>
+        <li><a href="">PHP</a></li>
+        <li><a href="">JAVASCRIPT</a></li>
+      </ul>  
+    </li>
+  </ul>
+</nav>
+</div>
+</div>
+        <div class="header">
+            <header>
+              <img src="../imagenes/icono.png" id="logo" width="200px" >
+                <center>
+                    <strong>
+                        <h1>PORTAFOLIO DE DESARROLLADOR</h1>
+                        <h4><a id="mi" href="http://localhost/ADSI2142329/portafolio/vista/perfil.php">Nathaly Carvajal</a></h4>
+                    </strong>
+                </center>
+            </header>
+        </div>
+    <div class="strong">
+        <strong class="strong">
+            <h2>Proyecto en ejecución</h2>
+        </strong>
+    </div>
+    <div class="div">
+        <p><strong>Domicilios Rappidin</strong><br><br>
+            Sitio Web donde cualquier persona puede encontrar cierta variedad 
+            de restaurantes que se encuentra en el municipio de Soacha (Cundinamarca) y a sus alrededores, 
+            cada uno de ellos tiene su propio menú que se puede ajustar al gusto y  presupuesto 
+            del cliente para realizar su pedido y entregar por el medio domiciliario</p>
+        <img src="../imagenes/DOMI.png">
+        <button><a href="http://localhost/formacion/DomiciliosRappidin/inicio.php"><strong>Visitar</strong></a></button>
+    </div>
+    <br>
+    <div class="div_section">
+        <div class="section">
+            <section class="manuales">
+                <center>
+                    <strong><h2>Manual de Usuario</h2></strong>
+                    <button><a href="manual_usuario.html">Más Información</a></button>
+                </center>
+            </section>
+        </div>
+        <br>
+        <div class="section">
+            <section class="manuales">
+                <center>
+                    <strong><h2>Manual del Software</h2></strong>
+                    <button><a href="manual_software.html">Más Información</a></button>
+                </center>
+            </section>
+        </div>
+        <br>
+        <div class="section">
+            <section class="github">
+                <center>
+                    <strong><h2>Repositorio github</h2></strong>
+                    <button><a href="https://github.com/kunvis/adsi2142329.git">Visitar</a></button>
+                </center>
+            </section>
+        </div>
+    </div>
+</body>
+<footer class="footer">
+    <strong><h2> sigueme en</h2></strong>
+    <img src="../imagenes/facebook.png" width="30px"><h3>Nathalie</h3>
+    <img src="../imagenes/instagram.png" width="30px"><h3>@2_nathalie_2</h3>
+
+</footer>
+</html>
+</body>
+</html>
---
+++ portafolio/vista/perfil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="../imagenes/icono.png" type="image/png"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portafolio</title>
+    <link rel="stylesheet" href="../estilo/perfil.css">
+</head>
+<body background="../imagenes/fondo1.jpg">
+    <center>
+        <div class="div_body">
+            <header>
+                <center>
+                    <strong>
+                        <h1>Desarrollador web</h1>
+                    </strong>
+                </center>
+              
+            </header>
+            <img src="../imagenes/fot.jpeg"  width="300px" >
+            <div class="div_article">
+                <article>
+                     
+                    <p>Bienvenid@<br>
+                        <br>
+                        Mi nombre es <strong>Nathaly carvajal </strong> <br>
+                        soy analista y desarrolladora egresada del sena <br>
+                        actualmente me encuatro cursando el sexto trimestre de mi tecnologo en 
+                    </p>
+                </article>
+            </div>
+        </div>
+    </center>
+    <br>
+    <center>
+        <button>
+            <a href="http://localhost/ADSI2142329/portafolio/vista/inicio.php">Regresar</a>
+        </button>
+    </center>
+</body>
+<footer class="footer">
+<strong><h2> sigueme en</h2></strong>
+    <img src="../imagenes/facebook.png" width="30px"><h3>Nathalie</h3>
+    <img src="../imagenes/instagram.png" width="30px"><h3>@2_nathalie_2</h3>
+</footer>
+</html
