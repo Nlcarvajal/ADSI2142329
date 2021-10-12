ef06456da62955358fe8063b86feb76345753589
Nlcarvajal <nlcarvajal0@misena.edu.co>
2021-10-11 22:15:52 -0500

    correcion de unos nombres de documentos de los manuales ya que estaban alcontrario ultima version portafolio

--- portafolio/vista/manuals.php
+++ portafolio/vista/manuals.php
@@ -6,106 +6,68 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>portafolio</title>
-    <link rel="stylesheet" href="../estilo/manualS.css">
+    <link rel="stylesheet" href="../estilo/manualU.css">
 </head>
     <header>
         <center>
-            <h1><strong>Manual de Usuario</strong></h1>
+            <strong>
+                <h1>Manual del Software</h1>
+            </strong>
         </center>
     </header>
     <div class="div">
-        <article>
-            <center>
-                <p>Crear software y productos con la calidad requerida para satisfacer 
-                    las necesidades de los usuarios y superar las expectativas de los 
-                    clientes más exigentes, casi siempre es nuestro objetivo primordial. 
-                    Pero, asesorar a los clientes y usuarios sobre el uso, comportamiento 
-                    y funcionamiento de nuestro software o producto, también es muy 
-                    importante, ya que ayuda a aclarar dudas y evitar confusiones sobre 
-                    la manera correcta de usar el software o producto que se oferta o vende. <br>
-                    Es por eso, que hablaremos de cómo elaborar un <strong>Manual de Usuario.</strong><br>
-                    <strong><u>¿Qué es un manual de usuario?</u></strong><br>
-                    Un manual de usuario o guía de usuario es un documento de comunicación 
-                    técnica destinado a dar asistencia a las personas que utilizan un sistema 
-                    en particular. Es decir, es un documento organizado y estructurado con 
-                    una serie de instrucciones sobre cómo utilizar un determinado sistema, 
-                    producto o servicio. <br>
-                <strong>El objetivo principal de un manual de usuario es y será lograr 
-                    una guía útil y entendible para cualquier usuario (principiante o avanzado).</strong></p>
-            </center>
-        </article>
-    </div>
-    <br>
-    <div class="div_div">
-        <div class="div">
+        <div class="div_article">
             <article>
                 <center>
-                    <strong><h2><u>Ventajas</u></h2></strong>
-                    <p>Las principales ventajas de crear un manual de usuario son:</p>
+                    <p>Un manual del software o técnico son aquellos que detalla los aspectos 
+                        técnicos e informáticos del sistema con la finalidad de explicar la 
+                        estructura del aplicativo al personal que quiera administrarlo, 
+                        editarlo o configurarlo. <br>
+                        A continuación, te daremos una breve explicación del paso a paso para 
+                        la creación de un manual de un software.</p>
                 </center>
-                <li>Son fáciles de usar y aprender.</li>
-                <li>Son una herramienta de apoyo en el entrenamiento y capacitación 
-                    del cliente o usuario.</li>
-                <li>No tienen costos.</li>
-                <li>Ayuda al usuario a aclarar dudas con la información que brinda.</li>
-                <li>Son un sumario de la totalidad de funciones y procedimientos de 
-                    un sistema, producto o servicio.</li>
-                <li>Brindan soluciones inmediatas a los diferentes problemas que se 
-                    pueden presentar.</li>
             </article>
         </div>
         <br>
-        <div class="div">
-            <article>
-                <center>
-                    <strong><h2><u>Desventajas</u></h2></strong>
-                    <p>Por supuesto, todo tiene sus pros y sus contras, algunas de sus 
-                    desventajas son:</p>
-                </center>
-                <li>Se corre el riesgo de que no sean leídos y usados por los usuarios o clientes.</li>
-                <li>Necesitará actualización constante sino pierde vigencia su contenido.</li>
-            </article>
-            <center>
-                <p>Entonces, para empezar a <strong>crear un buen manual de usuario</strong> 
-                sigue estos consejos </p>
-            </center>
+        <div class="div_section">
+            <section class="section_li">
+                <li><strong><u>OBJETIVO</u></strong> Dar a conocer el uso adecuado del software en aspectos 
+                    técnicos de manera descriptiva e ilustrada sobre los componentes 
+                    y funcionalidades que conforman el buen funcionamiento del 
+                    sistema de información.</li><br>
+                <li><strong><u>FINALIDAD DEL MANUAL</u></strong> La finalidad del manual técnico es instruir 
+                    a la persona que quiera administrar, editar o configurar el 
+                    software usando las debidas herramientas.</li>
+            </section>
         </div>
-    </div>
-    <br>
-    <div class="div_cuatro">
-        <article>
-            <center>
-                <p>Lo primero que tienes que hacer es saber quién leerá tu manual.<br>
-                    Conocer quién es tu audiencia o tu lector, es importante ya que te permite 
-                     escribir tu manual adaptándolo a sus necesidades.</p>
-                <p>Luego de conocer quién es tu audiencia, tienes que crear la estructura 
-                    que tendrá tu manual de usuario:</p>
-            </center>
-                <li><strong><u>PORTADA</u></strong> (nombre del manual, quien lo realizó y si los tiene, 
-                    un aviso de protección de derecho de autor).</li><br>
-                <li><strong><u>INTRODUCCIÓN</u></strong> (que describa brevemente para qué sirve y 
-                    de qué habla el manual).</li><br>
-                <li><strong><u>ÍNDICE DE CONTENIDO</u></strong> (para acceder rápidamente a los contenidos 
-                    del manual si es muy extenso).</li><br>
-                <li><strong><u>SECCIÓN CONFIGURACIÓN INICIAL</u></strong> (si el usuario necesita realizar 
-                    algún tipo de configuración).</li><br>
-                <li><strong><u>SECCIÓN DE CONTROL DE ACCESO</u></strong> (si es necesario usar una forma de 
-                    autenticación y describir los niveles de acceso del usuario 
-                    al que está dirigido el manual).</li><br>
-                <li><strong><u>SECCIÓN GUÍA O EXPLICACIÓN DEL FUNCIONAMIENTO DEL PRODUCTO, 
-                    SISTEMA O SERVICIO</u></strong> (se detalla por pasos lógicos y dividiendo 
-                    por secciones las diferentes funcionalidades del producto, 
-                    sistema o servicio).</li><br>
-                <li><strong><u>SECCIÓN PREGUNTAS FRECUENTES</u></strong> (se incluyen aquellas preguntas 
-                    frecuentes que pueden realizar los usuarios hacia los cuales 
-                    va dirigido el manual y sus respuestas).</li><br>
-                <li><strong><u>GLOSARIO DE TÉRMINOS</u></strong> (se aclararán todos aquellos términos 
-                    que puedan ser objeto de duda para el lector).</li>
-                <center>
-                    <p>Ahora que ya conoces cuáles son los elementos que debes tener en cuenta 
-                    para crear tu propio manual de usuario.</p>
-                </center>
-        </article>
+        <br>
+        <div class="div_section">
+            <section class="section_li">
+                <li><strong><u>INTRODUCCIÓN</u></strong> El manual se realiza con el fin de detallar el 
+                    software en términos técnicos para que la persona que vaya 
+                    administrar, editar o configurar el aplicativo lo haga de 
+                    una manera apropiada.</li><br>
+                <li><strong><u>DIAGRAMAS DE MODELAMIENTO</u></strong> Son los elementos que asocia diferentes 
+                    formas para formar diagramas que representan e Ilustra los aspectos 
+                    estáticos, dinámicos o estructurales del software y su hardware del sistema. <br>
+                    El documento se encuentra dividido en las siguientes secciones:
+                        <ol>DIAGRAMA DE CLASES</ol>
+                        <ol>DIAGRAMA DE CASOS DE USO</ol>
+                        <ol>DICCIONARIO DE DATOS</ol>
+                    </li>
+            </section>
+        </div>
+        <div class="div_section">
+            <section class="section_li">
+                <li><strong><u>ASPECTO TÉCNICO DEL DESARROLLO DEL SISTEMA</u></strong> Se realiza una descripción 
+                    detallada sobre los módulos y aspectos técnicos del aplicativo, relacionado 
+                    con la instalación de las herramientas necesarias para realizar modificaciones 
+                    requeridas de manera ordenada.</li><br>
+                <li><strong><u>REQUERIMIENTOS DEL SOFTWARE</u></strong> Especifica los requisitos mínimos del Hardware 
+                    para poder ejecutar los aplicativos usados para modificar el software.</li>
+            </section>
+        </div>
+        </div>
     </div>
     <br>
     <center>
@@ -119,4 +81,4 @@
     <img src="../imagenes/facebook.png" width="30px"><h3>Nathalie</h3>
     <img src="../imagenes/instagram.png" width="30px"><h3>@2_nathalie_2</h3>
 </footer>
-</html
+</html>
